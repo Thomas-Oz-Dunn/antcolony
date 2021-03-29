5565db886a3ae9b5f5c9be20a71ee711203cac49
Thomas-Oz-Dunn <thomasjohndunn@yahoo.com>
2021-03-29 12:16:04 -0500

    Added map generator

---
+++ main/generateMap.m
@@ -0,0 +1,5 @@
+function Map = generateMap(seed)
+    %Use Perlin noise to get pseudo random distribution for different features from seed number
+    Map = seed;
+    %Create a coastal area with fractal perimeter
+end
