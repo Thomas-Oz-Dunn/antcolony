d65051206ec7ef17dca3bbfe04b803352515bba5
Thomas-Oz-Dunn <thomasjohndunn@yahoo.com>
2021-03-28 21:53:09 -0500

    Leaving Trails

--- main/Ant.m
+++ main/Ant.m
@@ -2,7 +2,31 @@ classdef Ant
     properties
         xlocation {x}
         ylocation {y}
+        hasFound {boolean}
     end
     methods
+        
+        %Leave a pheremone every location been
+        function [markx,marky,pherVal] = leavePheremone(curx,cury,hasFound)
+            markx = curx;
+            marky = cury;
+            
+            %if hasfound = true
+            if hasFound == 1
+                %2 = yes direction
+                pherVal = 2;
+            else 
+                %1 = indifferent
+                pherVal = 1;
+            end
+        end
+        
+        %Move randomly until food pheremone found
+        function [newx,newy] = moveAnt(curx,cury,pherVal)
+            if pherVal = 1
+                newx = curx;
+                newy = cury;
+            else
+        end
     end
 end
--- main/mainMapcode.m
+++ main/mainMapcode.m
@@ -6,3 +6,8 @@ WorldMap = zeros(100,100);
 a = Ant;
 a.xlocation = 1;
 a.ylocation = 5;
+
+if WorldMap(a.xlocation,a.ylocation) == 1
+    a.hasfound = true;
+end
+
