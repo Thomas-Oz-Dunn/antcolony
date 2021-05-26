83b84ae3207100e5b78b84f78fb5561c5165487a
Thomas-Oz-Dunn <thomasjohndunn@yahoo.com>
2021-05-26 13:08:00 -0500

    started pseudocode

--- main/AntClass.py
+++ main/AntClass.py
@@ -1,7 +1,21 @@
 class Ant:
 	"an ant class"
-	def __init__(self,foodstatus,location)
+	def __init__(self,foodstatus,location,energy)
 
-	
 		self.foodstatus = foodstatus
 		self.location = location
+		self.energy = energy
+
+		"See environment of a row of 3 spaces ahead"
+		"Detect food or food trail"
+		"Detect wall"
+		"Move towards food trail or food plus random wobble"
+		"Decrement energy"
+		"If food, take or eat food, depending on energy level"
+		"If no food or food trail, move in random direction forward"
+		"If no food, Leave return trail at previous location"
+		"If take food, change trail type to food trail"
+		"If take food, see environment, find return trail, move to return trail, leave food trail"
+		"Drop food off at colony"
+		"Eat food to replenish health"
+		"Follow food trail back"
--- main/Map.py
+++ main/Map.py
@@ -1,3 +1,16 @@
+import ant
+
 class Map
 	"an environment class"
 	def __init__(self,size,walls,food_locations)
+
+	self.size = size
+	self.walls = walls
+	self.food_locations = food_locations
+
+	"input dimensions of map"
+	"Run perlin noise to generate random pockets of rock"
+	"overlay multiple for cave system gradient"
+	"assign walls based on border between black and white"
+	"Run perlin noise to generate random pockets of food"
+	"If open air & food, food @ coordinates"
